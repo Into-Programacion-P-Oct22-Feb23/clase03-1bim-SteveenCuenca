d0d334b4a10b0d65349d347f50e79fc23d6d03a6
Steven Cuenca <626tellor@gmail.com>
2022-10-26 10:51:31 -0500

    Ejercicios en Netbeans

---
+++ ejemplos-IDE/Ejemplo01/src/ejemplo01/Ejemplo03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author SALA I
+ */
+public class Ejemplo03 {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo 03");
+    }
+}
+                    
+       
+
